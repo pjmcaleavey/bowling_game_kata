038c99a485259c98e6c4e0a8c65d487b364886b9
The Learning Team <thelearningteam+amorgan2+jschiavone+pmcaleavey@manheim.com>
2016-08-15 11:17:36 -0400

    passed tests 1 and 2

--- game.rb
+++ game.rb
@@ -1,13 +1,23 @@
-class Game
+require 'pry'
 
-  def roll(pins)
-    #called each time a player rolls a ball
-    #argument is the number of pins knocked down
-    return pins
+class Game
+  def initialize
+    @previous_rolls = []
   end
 
-  # def score
-  #   #called at the end of a game. return total score for that game
-  #   #must iterate thru all the frames & calculate the score
-  # end
+  def roll(pins)
+    @previous_rolls << pins
+    #called each time a player rolls a ball
+    #argument is the number of pins knocked down
+  end
+
+  def score
+    #binding.pry
+    sum_of_rolls = 0
+    @previous_rolls.each { |rolls| sum_of_rolls += rolls}
+
+    return sum_of_rolls
+    #called at the end of a game. return total score for that
+    #game must iterate thru all the frames & calculate the score
+  end
 end
--- spec/bowling_tests.rb
+++ spec/bowling_tests.rb
@@ -3,14 +3,32 @@ require './game' # require_relative 'game' - also works
 
 describe 'Game' do
 
-  it 'creates a game object' do
-    @game = Game.new
-    expect(@game).to be_instance_of(Game) # be_kind_of - also works
+
+  it 'can score a gutter ball game' do
+    game = Game.new
+    20.times do
+      game.roll(0)
+    end
+    expect(game.score).to eq(0)
   end
 
- it 'rolls the bowling ball' do
-    @game.roll(5)
-    expect(@game.roll(5)).to eq(5)
- end
+  it 'can score a game with 1 in each frame' do
+    game = Game.new
+    20.times do
+      game.roll(1)
+    end
+    expect(game.score).to eq(20)
+  end
+
+  # it 'rolls one spare' do
+  #   game = Game.new
+  #   game.roll(5)
+  #   game.roll(5)
+  #   game.roll(3)
+  #   17.times do
+  #     game.roll(0)
+  #   end
+  #   expect(game.score).to eq(16)
+  # end
 
 end
